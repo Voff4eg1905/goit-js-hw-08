0bd9bd29a0a9f3a1465710c8d168aecc3cef823c
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2023-01-08 22:28:59 +0200

    fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,38 +6,36 @@ fillTheForm();
 const onInputChange = event => {
   const { name, value } = event.target;
   userData[name] = value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
-
   writeToLocalStorage();
-  // console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
 };
 const writeToLocalStorage = () => {
   localStorage.setItem('feedback-form-state', JSON.stringify(userData));
 };
 
 function fillTheForm() {
-    
-    try {
-        const userInfoForm = JSON.parse(localStorage.getItem('feedback-form-state'));
-        if (userInfoForm === null) {
-            return;
-        }
-        for (const field in userInfoForm) {
-            formEl.elements[field].value = userInfoForm[field];
-        }
-        
-    } catch (error) {
-        console.log(error);
+  try {
+    const userInfoForm = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+    if (userInfoForm === null) {
+      return;
     }
- 
 
+    Object.entries(userInfoForm).forEach(([name, value]) => {
+      userData[name] = value;
+      formEl.elements[name].value = value;
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
+
 const onSubmit = event => {
   event.preventDefault();
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   formEl.reset();
   userData = {};
   localStorage.removeItem('feedback-form-state');
 };
+
 formEl.addEventListener('input', throttle(onInputChange, 500));
 formEl.addEventListener('submit', onSubmit);
